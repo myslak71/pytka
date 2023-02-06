c89a54cbbd070d4e91d6b97e6fc394904da15c52
myslak <myslak@protonmail.com>
2023-02-06 12:27:19 +0100

    Initial commit

---
+++ pytka/__init__.py
@@ -0,0 +1,29 @@
+[build-system]
+requires = ["setuptools", "setuptools-scm"]
+build-backend = "setuptools.build_meta"
+
+[project]
+name = "pytka"
+version = "0.0.0"
+description = "Just simple pytka."
+authors = [{ name = "myslak71", email = "myslak@protonmail.com" }]
+maintainers = [{ name = "myslak71", email = "myslak@protonmail.com" }]
+readme = "README.md"
+requires-python = ">=3.11"
+license = { file = "LICENSE" }
+keywords = [
+  "polish",
+  "vat",
+  "client",
+  "api",
+]
+classifiers = [
+  "Development Status :: 3 - Alpha",
+  "Environment :: Console",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  "Programming Language :: Python",
+  "Programming Language :: Python :: 3.11",
+  "Programming Language :: Python :: 3 :: Only",
+]
+urls = { repository = "https://github.com/myslak71/vater" }
